5a182f88c17d1380159b22c6310385aad5a58fc1
Jason Kuphaldt <jasonkuphaldt@howeneat.com>
2023-10-12 13:32:42 -0700

    adding a catch for parsing bad JSON

--- server/index.js
+++ server/index.js
@@ -45,6 +45,18 @@ let app = Express();
 app.use(BodyParser.urlencoded({ extended: true }));
 app.use(BodyParser.json());
 app.use(BodyParser.raw());
+app.use((err, req, res, next) => {
+    if (err) {
+        let now = new Date(); // Get the date/time
+        let m_date = new Date(now.toISOString()); // Convert to ISO format
+        console.log("\n" + m_date + " - BAD post req from " + req.ip + " || Url: " + req.url);
+        console.log('Error occurred parsing data. This usually means bad JSON was sent. Sending timestamp as a response.')
+        console.log({t: Math.floor(Date.now() / 1000)})
+        res.send({t: Math.floor(Date.now() / 1000)})
+    } else {
+        next()
+    }
+})
 
 let database, collection;
 
