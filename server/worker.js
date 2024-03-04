25844154805c6d6c18f673c7be0f278e0183aeb3
Jason Kuphaldt <jasonkuphaldt@howeneat.com>
2024-03-04 11:15:21 -0800

    just logging filtered_docs

--- server/worker.js
+++ server/worker.js
@@ -299,16 +299,18 @@ queue.process(async (job, done) => {
         }
 
         // //This block of code filters out duplicate data
-        // const promises = docArray.map(async(doc) => {
-        //     const check = await collection.findOne(doc)
-        //     return {
-        //         value: doc,
-        //         include: check == null
-        //     }
-        // })
-        // const data_with_includes = await Promise.all(promises)
-        // const filtered_data_with_includes = data_with_includes.filter(v => v.include)
-        // const filtered_docs = filtered_data_with_includes.map(data => data.value)
+        const promises = docArray.map(async(doc) => {
+            const check = await collection.findOne(doc)
+            return {
+                value: doc,
+                include: check == null
+            }
+        })
+        const data_with_includes = await Promise.all(promises)
+        const filtered_data_with_includes = data_with_includes.filter(v => v.include)
+        const filtered_docs = filtered_data_with_includes.map(data => data.value)
+
+        console.log('filtered_docs', filtered_docs)
 
         await collection.insertMany(docArray, (error, result) => {
             console.log(result)
