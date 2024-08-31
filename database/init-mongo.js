e6065cc2ba0046ebb3cdebe917af3306f068da08
Daniel Howe <daniel@howeneat.com>
2024-08-31 10:36:33 -0700

    Updated initialization script

--- database/init-mongo.js
+++ database/init-mongo.js
@@ -11,22 +11,15 @@ const DATABASE_COLLECTION = 'device-data';
 //// SETUP DATABASE                       ////
 //////////////////////////////////////////////
 db = new Mongo().getDB(DATABASE_NAME);
-db.createUser({
-    user: "mongodb-user",
-    pwd: "mongodb-user-password",
-    roles: [
-        {
-            role: "readWrite",
-            db: "neatmon_dev"
-        }
-    ]
-});
 
 //////////////////////////////////////////////
 //// CREATE A COLLECTION                //////
 //////////////////////////////////////////////
-// db.createCollection('device-data', { capped:true, size:3000000000 });
 
+// devicetimeseriesdatas is where all device data is stored
+// the timeseries type of collection is specialized for data from IoT devices
+// https://www.mongodb.com/docs/manual/core/timeseries-collections/
+// note this requires version 5.0 or greater
 db.createCollection(
     "devicetimeseriesdatas",
     {
@@ -37,3 +30,22 @@ db.createCollection(
        }
     }
 )
+
+/**
+ * To create a capped/cache collection that automatically purges data 
+ * see the guide from MongoDB https://www.mongodb.com/docs/manual/core/capped-collections/create-capped-collection/#std-label-create-capped-collection-max-size
+ * uncomment the function below, and comment out the createCollection above
+ */
+// db.createCollection(
+//     "devicetimeseriesdatas",
+//     {
+//        timeseries: {
+//           timeField: "timestamp",
+//           metaField: "metadata",
+//           granularity: "hours"
+//        },
+//        capped: true,
+//        size: 100000,
+//        max: 5000
+//     }
+// )
